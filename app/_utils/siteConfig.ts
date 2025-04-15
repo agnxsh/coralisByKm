c78949c2f0d1248edb0edf246b64df706842f768
Roho7 <rohosen2@gmail.com>
2025-04-15 16:58:20 +0530

    add [site config and og_image]

---
+++ app/_utils/siteConfig.ts
@@ -0,0 +1,19 @@
+export const siteConfig = () => {
+  return {
+    title: 'Coralis',
+    description: 'Explore the Magnificent',
+    baseUrl: 'https://coralis.com',
+    ogImage: '/og-image.png',
+    twitter: '@coralis',
+    icons: {
+      icon: '/favicon.ico',
+    },
+    themeColor: '#000000',
+    viewport: {
+      width: 'device-width',
+      initialScale: 1,
+      maximumScale: 1,
+    },
+    name: 'Coralis',
+  };
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 
+import { Metadata } from 'next';
 import { Montserrat, Playfair_Display } from 'next/font/google';
+import { siteConfig } from './_utils/siteConfig';
 import './globals.css';
 
 const playfair = Playfair_Display({ 
@@ -14,10 +16,33 @@ const montserrat = Montserrat({
   variable: '--font-montserrat',
 });
 
-export const metadata = {
-  title: 'Coralis',
-  description: 'Explore the Magnificent',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: siteConfig().title,
+    description: siteConfig().description,
+    openGraph: {
+      title: siteConfig().title,
+      description: siteConfig().description,
+      type: 'website',
+      url: siteConfig().baseUrl,
+      images: [
+        {
+          url: siteConfig().ogImage,
+          width: 1200,
+          height: 630,
+          alt: siteConfig().name,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteConfig().title,
+      description: siteConfig().description,
+      images: [siteConfig().ogImage],
+      creator: siteConfig().twitter,
+    },
+  };
+}
 
 export default function RootLayout({
   children,
